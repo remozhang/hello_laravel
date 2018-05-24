ca8cb83d4951648134b14334ece7b9b698356b9c
remozhang <814895077@qq.com>
2018-05-24 00:57:37 +0000

    finish 5.23

--- larabbs/routes/web.php
+++ larabbs/routes/web.php
@@ -16,3 +16,22 @@
 //});
 
 Route::get('/', 'PagesController@root')->name('root');
+// 此处是laravel的用户认证路由
+//Auth::routes();
+
+// 以下等同于Auth::routes();
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@loginout')->name('logout');
+
+// Registration Routes..
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResultForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+//Route::get('/home', 'HomeController@index')->name('home');
