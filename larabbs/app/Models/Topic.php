54f8d67baabce795f9bb5bd64210e4f2b0cecd5d
remozhang <814895077@qq.com>
2018-06-07 07:27:26 +0000

    Seeded users and topics data

--- larabbs/app/Models/Topic.php
+++ larabbs/app/Models/Topic.php
@@ -4,5 +4,6 @@ namespace App\Models;
 
 class Topic extends Model
 {
+    // 这里是允许这些数据填充
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
 }
--- larabbs/database/factories/TopicFactory.php
+++ larabbs/database/factories/TopicFactory.php
@@ -3,7 +3,19 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+    $sentence = $faker->sentence();
+
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'updated_at' => $updated_at,
+        'created_at' => $created_at,
         // 'name' => $faker->name,
     ];
 });
