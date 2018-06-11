6e7375dd5242c48705b9ed7692b61f6a3a3bf61f
remozhang <814895077@qq.com>
2018-06-11 09:33:21 +0000

    list topics which was created by users themself

--- larabbs/app/Models/User.php
+++ larabbs/app/Models/User.php
@@ -26,4 +26,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     *
+     * 这里用户与话题是一对多关系，一个用户拥有多个主题，在Eloquent中使用hasMany()方法进行关联
+     * 关联设置成功后,我们既可以使用$user->topics来获取用户发布的所有话题数据
+     */
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+
 }
