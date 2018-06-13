0b04ac9a6140500064330616ffb06daaecdb98a2
remozhang <814895077@qq.com>
2018-06-13 09:10:45 +0000

    User can upload topic image

--- larabbs/resources/views/topics/create_and_edit.blade.php
+++ larabbs/resources/views/topics/create_and_edit.blade.php
@@ -13,7 +13,15 @@
     <script>
         $(document).ready(function(){
             var editor = new Simditor({
-                textarea: $('#editor')
+                textarea: $('#editor'),
+                upload: {
+                    url: '{{ route("topics.upload_image") }}',
+                    params: { _token: '{{ csrf_token() }}' },
+                    fileKey: 'upload_file',
+                    connectionCount: 3,
+                    leaveConfirm: '文件上传中，关闭此页面将停止上传。'
+                },
+                pasteImage: true,
             });
         })
     </script>
